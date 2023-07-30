bf6dce34b3ecf03ce6fb1a905de48498a500cac9
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-30 10:13:02 -0700

    while loop number

--- while.js
+++ while.js
@@ -19,3 +19,10 @@ while (num1 < 5) {
     num1++;
     console.log("I love Bangldesh");
 }
+
+/**
+ * 1. loop variable 
+ * 2. condition inside while
+ * 3. Loop body
+ * 4. change the loop variable 
+ */
