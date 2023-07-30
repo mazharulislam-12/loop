6cda51e0b33f4471de3b1f9172786c5397d6f125
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-30 08:38:17 -0700

    revesion array and varible

---
+++ revison.js
@@ -0,0 +1,17 @@
+// string type variable
+var bottle = 'water';
+var bottleColor = 'yellow';
+var penColor = 'blue';
+// number type variable 
+var sunglassPrice = 120;
+// boolean type variable 
+var isExpensive = true;
+
+// array
+var tableItems = ['bottle', 'sunglass', 'pen', 'mouse', 'paper', 'book']
+var item3 = tableItems[3];
+tableItems[4] = 'notebook';
+var penIndex = tableItems.indexOf('pen');
+
+// push and pop
+// condition
