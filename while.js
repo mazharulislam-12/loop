cdb3a0b62e1911f5fecc1d9fcb464812565271b5
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-30 09:04:49 -0700

    while loop for js

---
+++ while.js
@@ -0,0 +1,21 @@
+var rostGiven = 0; 
+while (rostGiven < 7) {
+    // rostGiven = rostGiven + 1;
+    // rostGiven += 1;
+    rostGiven++;
+    console.log("rost den" );
+    console.log(rostGiven);
+}
+
+var num = 0;
+while (num < 500) {
+    num++;
+    console.log("Hello world");
+    // console.log(num);
+}
+
+var num1 = 0;
+while (num1 < 5) {
+    num1++;
+    console.log("I love Bangldesh");
+}
