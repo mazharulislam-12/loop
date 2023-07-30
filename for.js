d78269db8e6d66ae8315f1a537d4fd09c9d36cfb
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-30 10:48:43 -0700

    for loop js

---
+++ for.js
@@ -0,0 +1,31 @@
+// for loop 
+for(var rostGiven = 0; rostGiven < 7; rostGiven++){
+    console.log("Rost den Please !!");
+}
+
+for(var num = 0; num < 10; num++){
+    console.log(num);
+}
+
+// simple version of for loop
+for(var i = 0; i < 7; i++){
+    console.log(i);
+}
+ 
+for(var i = 0 ; i <= 10; i++){
+    console.log(i);
+}
+
+
+// for loop even 
+console.log("for loop even");
+for (var i = 0; i<= 10; i = i+2){
+    console.log(i);
+}
+
+
+// for loop odd
+console.log("for loop odd");
+for (var i = 1; i <= 10; i = i+2){
+    console.log(i);
+}
