410010d8863d9aff3bda00da75332516acad1bcc
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-30 11:43:19 -0700

    array and loop for js

---
+++ array-loop.js
@@ -0,0 +1,16 @@
+// target : display evenly elements of an array
+var numbers = [45, 56, 96, 82, 42, 32, 66, 41, 85, 96];
+
+for(var i = 0; i < numbers.length; i++){
+    var number = numbers[i]
+    console.log(number);
+}
+
+
+var items = ['bottle', 'mouse', 'sunglass', 'pen', 'book',];
+for(i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+
+
+
