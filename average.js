a47c9dd82d96eeb45b0187fb5596186e3cf16c9d
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-30 21:59:48 -0700

    reminder for js

---
+++ average.js
@@ -0,0 +1,12 @@
+
+var mathMarks = 72.25;
+var biologyMarks = 65;
+var chemistryMarks = 80;
+var physicsMarks = 35.45;
+var banglaMarks = 99.50;
+
+
+var totalMarks = mathMarks + biologyMarks + chemistryMarks + physicsMarks + banglaMarks;
+console.log(totalMarks);
+var average = totalMarks / 5;
+console.log(average);
---
+++ string-concationation.js
@@ -0,0 +1,11 @@
+var fastInput = "I am going to be"
+var secondInput = "an awesome web developer"
+var fastAndSecond = fastInput + " " secondInput;
+console.log(fastAndSecond);
+
+
+
+const str1 = "Hello";
+const str2 = "World";
+
+const result = str1 + " " + str2; // "Hello World"
