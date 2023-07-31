4a300c9c0bf9cfbedf7d23a252312930b44fd03d
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-30 22:50:13 -0700

    icoseles for js

---
+++ icoseles.js
@@ -0,0 +1,9 @@
+var side1 = 9;
+var side = 8;
+var side3 = 9;
+if (side1 == side2 || side1 == side3 || side2 == side3) {
+    console.log("icosloes");
+}
+else{
+    console.log("not isoceles");
+}
---
+++ largest.js
@@ -0,0 +1,4 @@
+var number1 = 13;
+var number2 = 79;
+var number3 = 45;
+ 
